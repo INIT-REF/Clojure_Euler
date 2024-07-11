201ef3a57da5217936e680915100edae166c03e9
INIT-REF <rufmichan@freenet.de>
2024-07-11 11:12:45 +0200

    Update

--- 005.clj
+++ 005.clj
@@ -1,1 +1,7 @@
 (defn gcd [a, b] (if (= a 0) b (recur (rem b a) a)))
+
+(defn lcm [a, b] (quot (* a b) (gcd a b)))
+
+(defn euler005 [limit] (reduce lcm 1 (range 2 (inc limit))))
+
+(println (euler005 20))
