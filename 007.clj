3b39c627ae1fa3e6a227f0b30fd7cb0bb3f5036e
INIT-REF <rufmichan@freenet.de>
2024-07-11 12:38:38 +0200

    Update

---
+++ 007.clj
@@ -0,0 +1,14 @@
+(defn sieve [limit] (into [] (map #(< 1 %) (range 0 (inc limit)))))
+
+(defn SoE
+  ([limit] (SoE limit (sieve limit) 2 4))
+  ([limit, s, d, m]
+   (if (> (* d d) limit)
+     s
+     (if (or (> m limit) (not (nth s d)))
+       (recur limit s (inc d) (* (inc d) (inc d)))
+       (recur limit (assoc s m false) d (+ m d))))))
+
+(defn primes [limit] (->> (range 0 (inc limit)) (map #(if %1 %2) (SoE limit)) (remove nil?)))
+
+(println (nth (primes 114500) 10000)
