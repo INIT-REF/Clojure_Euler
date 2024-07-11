53bdffda8a167013774fa7a8f3892ed09e10bdae
INIT-REF <rufmichan@freenet.de>
2024-07-11 09:42:28 +0200

    Update

--- 002.clj
+++ 002.clj
@@ -1,10 +1,10 @@
-(defn fib
-  ([n] (fib '(2 1) n))
+(defn fibto
+  ([n] (fibto '(2 1) n))
   ([x, n]
    (if (< (first x) n)
-     (fib (cons (+ (first x) (first (rest x))) x) n)
+     (fibto (cons (+ (first x) (first (rest x))) x) n)
      (rest x))))
 
-(defn euler002 [x] (reduce + 0 (filter even? (fib x))))
+(defn euler002 [x] (reduce + 0 (filter even? (fibto x))))
 
 (println (euler002 4000000))
