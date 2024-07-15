01a9bf9016b2549b9cff1669c3c95f0d7cf0dcdd
INIT-REF <rufmichan@freenet.de>
2024-07-15 05:45:03 +0200

    Update

--- 002.clj
+++ 002.clj
@@ -1,10 +1,10 @@
-(defn fibto
-  ([n] (fibto '(2 1) n))
-  ([x, n]
-   (if (< (first x) n)
-     (recur (cons (+ (first x) (first (rest x))) x) n)
-     (rest x))))
-
-(defn euler002 [x] (->> (fibto x) (filter even?) (reduce +)))
+(defn euler002
+  ([n] (euler002 n 1 2 0))
+  ([limit, f1, f2, evensum]
+   (if (> f2 limit)
+     evensum
+     (if (even? f2)
+       (recur limit f2 (+ f2 f1) (+ evensum f2))
+       (recur limit f2 (+ f2 f1) evensum)))))
 
 (println (euler002 4000000))
