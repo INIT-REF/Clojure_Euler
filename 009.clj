04f8c4cdc77f4ee539a681d4bad7c56d405d2cab
INIT-REF <rufmichan@freenet.de>
2024-07-15 20:58:35 +0200

    Update

--- 009.clj
+++ 009.clj
@@ -1,15 +1,16 @@
-(defn per [r s t] (+ (+ r s) (+ r t) (+ r s t)))
-(defn prod [r s t] (* (+ r s) (+ r t) (+ r s t)))
-(defn rs2 [r] (quot (* r r) 2))
+(defn per [[r s t]] (+ (+ r s) (+ r t) (+ r s t)))
+(defn prod [[r s t]] (* (+ r s) (+ r t) (+ r s t)))
 
 (defn euler009
   ([] (euler009 2 1))
   ([r d]
-   (if (> (* d d) (rs2 r))
-     (recur (+ r 2) 1)
-     (if (or (not= 1000 (per r d (quot (rs2 r) d)))
-             (> (rem (rs2 r) d) 0))
-     (recur r (+ d 1))
-     (prod r d (quot (rs2 r) d))))))
+   (let [rs2 (quot (* r r) 2)]
+     (if (> (* d d) rs2)
+       (recur (+ r 2) 1)
+       (let [rst [r d (quot rs2 d)]]
+         (if (or (not= 1000 (per rst))
+             (> (rem rs2 d) 0))
+           (recur r (+ d 1))
+           (prod rst)))))))
 
 (println (euler009))
